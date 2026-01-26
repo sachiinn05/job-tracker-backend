3d83e96c868df5d78637601c6c81a5b72e4d35ce
sachinsingh <sachinsingh6386@gmail.com>
2026-01-26 18:25:21 +0530

    Initial backend setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const connectDB=require("./config/database")
+const app=express();
+
+app.use(express.json());
+
+connectDB()
+.then(()=>{
+    console.log("Database connection established..");
+    app.listen(9001,()=>{
+       console.log("Server listening on port 9000"); 
+    }) 
+})
+.catch((err)=>{
+     console.log("Database cannot be connected..", err);
+
+})
