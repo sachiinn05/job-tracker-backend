7b1a32863ac5a6bf6ad9422723e0be039c79ed98
sachinsingh <sachinsingh6386@gmail.com>
2026-01-30 20:43:27 +0530

    editi uploads

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,28 +1,27 @@
-const multer=require("multer");
+const multer = require("multer");
 
-const storage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"uploads/");
-    },
-    filename:(req,file,cb)=>{
-        cb(null,Data.now()+" "+file.originalname);
-    }
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
 });
 
-const upload=multer({
-    storage,
-    limitsL:{fileSize:2*1024*1024}, 
-    fileFilter:(req,file,cb)=>{
-        if(
-            file.mimetype==="image/jpeg" ||
-            file.mimetype==="image/png"
-        )
-        {
-            cb(null,true);
-        }else{
-            cd(new Error ("Only JPG and PNG files allowed",false));
-        }
+const upload = multer({
+  storage,
+  limits: { fileSize: 2 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/png"
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only JPG and PNG files allowed"), false);
     }
+  }
+});
 
-})
-module.exports=upload;
+module.exports = upload;
