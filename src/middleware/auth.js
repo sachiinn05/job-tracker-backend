af1d93c26f0051a07d755ab7932348d2eaa7d4d8
sachinsingh <sachinsingh6386@gmail.com>
2026-01-30 18:23:43 +0530

    add auth.js  for token verify

---
+++ src/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt=require("jsonwebtoken");
+const User=require("../models/user");
+
+const userAuth=async (req,res,next)=>{
+    try{
+        const {token}=req.cookies;
+        if(!token)
+        {
+            return res.status(401).send("Please Login !!");
+        }
+     const decodeObj=await jwt.verify(token,"DEV@JobTracker390");
+     const {_id}=decodeObj;
+     const user= await User.findById(_id);
+     if(!user)
+     {
+        return res.status(401).send("User not found");
+     }
+     req.user=user;
+     next();
+
+    }catch(err){
+      res.status(400).send("Error : "+err.message);
+    }
+}
+module.exports={userAuth};
