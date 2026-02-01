696803e427afb24ecb9dccbb9ff8178e48b5296e
sachinsingh <sachinsingh6386@gmail.com>
2026-02-01 18:44:57 +0530

    add more job apis

--- src/routes/job.js
+++ src/routes/job.js
@@ -5,14 +5,24 @@ const JobApplication = require("../models/jobApplication");
 
 jobRouter.post("/job",userAuth,async(req,res)=>{
    try{
-      const job=new JobApplication ({
+    const currentStatus = req.body.currentStatus || "Applied";
+
+    const job = new JobApplication({
       userId: req.user.id,
       companyName: req.body.companyName,
       role: req.body.role,
       platform: req.body.platform,
       jobLink: req.body.jobLink,
-      notes: req.body.notes
-      });
+      currentStatus,
+      notes: req.body.notes,
+      statusHistory: [
+        {
+          status: currentStatus,
+          date: new Date(),
+          note: "Initial application"
+        }
+      ]
+    });
       await job.save();
       res.status(200).json(job);
    }
@@ -21,5 +31,15 @@ jobRouter.post("/job",userAuth,async(req,res)=>{
      res.status(400).send("Error :"+err.message);
    }
 });
+
+jobRouter.get("/job",userAuth, async(req,res)=>{
+    try{
+        const jobs=await JobApplication.find({userId:req.user.id});
+        res.status(200).json(jobs);
+    }catch(err)
+    {
+        res.status(400).send("Error :"+err.message);
+    }
+})
 
 module.exports=jobRouter;
